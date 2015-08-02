f73adb490d32a81c6544e37c84b2d2216a386d51
Artemenko Vladimir <artemenko.vladimi@gmail.com>
2015-08-02 21:10:30 +0300

    Added Task3

---
+++ Task3.Lib/CustomQueue.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task3.Lib
+{
+	public class CustomQueue<T>
+	{
+		#region private members
+		private T[] queue;
+		private const int _defaultCapasity = 10;
+		private int _head;
+		private int _tail;
+		#endregion
+
+		#region properties
+		public int Count { get { return _tail - _head + 1; } }
+		#endregion
+
+		#region ctors
+		public CustomQueue ()
+		{
+			_head = 0;
+			_tail = -1;
+			queue = new T[_defaultCapasity];
+		}
+		#endregion
+
+		#region public methods
+		public void Enqueue (T item)
+		{
+			ResizeQueue ();
+
+			_tail++;
+			queue [_tail] = item;
+		}
+
+		public T Dequeue ()
+		{
+			if (Count == 0)
+				throw new InvalidOperationException ("Queue is empty");
+
+			var res = queue [_head];
+			queue [_head] = default(T);
+			_head++;
+
+			return res;
+		}
+
+		public T Peek ()
+		{
+			if (Count == 0)
+				throw new InvalidOperationException ("Queue is empty");
+
+			return queue [_head];
+		}
+			
+		public CustomQueueIterator GetEnumerator ()
+		{
+			return new CustomQueueIterator (this);
+		}
+		#endregion
+
+		#region Iterator
+		public class CustomQueueIterator
+		{
+			private readonly CustomQueue<T> queue;
+			private int curIndex = -1;
+
+			public CustomQueueIterator (CustomQueue<T> queue)
+			{
+				this.queue = queue;
+			}
+
+			public bool MoveNext ()
+			{
+				return ++curIndex < queue.Count;
+			}
+
+			public void Reset ()
+			{
+				curIndex = -1;
+			}
+				
+			public T Current {
+				get {
+					if (curIndex == -1 || curIndex == queue.Count)
+						throw new InvalidOperationException ();
+					
+					return queue.queue [curIndex];
+				}
+			}
+		}
+		#endregion
+
+		#region private methods
+		private void ResizeQueue ()
+		{
+			if (_tail >= queue.Length - 1) {
+				var tmp = new T[Count * 2];
+				queue.CopyTo (tmp, _head);
+				queue = tmp;
+				_head = 0;
+				_tail = Count - 1;
+			}
+		}
+		#endregion
+	}
+}
+
---
+++ Task3.Tests/Test.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Task3.Lib;
+
+namespace Task3.Tests
+{
+	[TestFixture ()]
+	public class Test
+	{
+		private CustomQueue<int> queueInt;
+
+		[SetUp]
+		public void Setup()
+		{
+			queueInt = new CustomQueue<int> ();
+			queueInt.Enqueue (1);
+			queueInt.Enqueue (3);
+			queueInt.Enqueue (5);
+		}
+
+		[Test ()]
+		public void TestLength ()
+		{
+			Assert.AreEqual (3, queueInt.Count);
+		}
+
+		[Test ()]
+		public void TestLengthAfterDequeue ()
+		{
+			queueInt.Enqueue (7);
+			queueInt.Enqueue (9);
+			queueInt.Dequeue ();
+			Assert.AreEqual (4, queueInt.Count);
+		}
+
+		[Test ()]
+		public void TestPeek1 ()
+		{
+			queueInt.Enqueue (7);
+			queueInt.Enqueue (9);
+			queueInt.Dequeue ();
+			Assert.AreEqual (3, queueInt.Peek ());
+		}
+
+		[Test ()]
+		public void TestPeek2 ()
+		{
+			queueInt.Enqueue (9);
+			queueInt.Dequeue ();
+			queueInt.Dequeue ();
+			queueInt.Dequeue ();
+			Assert.AreEqual (9, queueInt.Peek ());
+		}
+
+		[Test ()]
+		[ExpectedException ()]
+		public void TestPeekException ()
+		{
+			queueInt.Dequeue ();
+			queueInt.Dequeue ();
+			queueInt.Dequeue ();
+			Assert.AreEqual (9, queueInt.Peek ());
+		}
+
+		[Test ()]
+		public void TestIterator ()
+		{
+			var list = new List<int> ();
+			foreach (var i in queueInt) {
+				list.Add (i);
+			}
+			Assert.AreEqual (3, list.Count);
+		}
+	}
+}
+
