32d5dae8c223f69b99f99f54939fec15ece7107e
Artemenko Vladimir <artemenko.vladimi@gmail.com>
2015-08-02 17:11:00 +0300

    Added Task2

---
+++ Task2.Lib/Fibonacci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2.Lib
+{
+	public static class Fibonacci
+	{
+		public static IEnumerable<int> GetSequence (int n)
+		{
+			if (n < 1)
+				throw new ArgumentException ();
+			
+			int a = 0, b = 1, temp;
+			yield return b;
+			for (int i = 1; i < n; i++) {
+				temp = a;
+				a = b;
+				b = temp + b;
+				yield return b;
+			}
+		}
+	}
+}
+
---
+++ Task2.Tests/Test.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Task2.Lib;
+
+namespace Task2.Tests
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase1 ()
+		{
+			Assert.AreEqual (4, Fibonacci.GetSequence (3).Sum ());
+		}
+
+		[Test ()]
+		public void TestCase2 ()
+		{
+			Assert.AreEqual (7, Fibonacci.GetSequence (4).Sum ());
+		}
+
+		[Test ()]
+		public void TestCase3 ()
+		{
+			Assert.AreEqual (20, Fibonacci.GetSequence (6).Sum ());
+		}
+
+		[Test ()]
+		public void TestCase4 ()
+		{
+			Assert.AreEqual (33, Fibonacci.GetSequence (7).Sum ());
+		}
+
+		[Test ()]
+		public void TestCase5 ()
+		{
+			Assert.AreEqual (54, Fibonacci.GetSequence (8).Sum ());
+		}
+	}
+}
+
