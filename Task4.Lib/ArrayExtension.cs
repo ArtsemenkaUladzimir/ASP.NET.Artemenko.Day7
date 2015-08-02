7a6e81fdeee702bca37d4db560e927e3a4005a02
Artemenko Vladimir <artemenko.vladimi@gmail.com>
2015-08-02 23:44:13 +0300

    Task4 added

---
+++ Task4.Lib/ArrayExtension.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task4.Lib
+{
+	public static class ArrayExtension
+	{
+		#region public methods
+
+		public static int BinarySearch<T> (T[] array, T value)
+		{
+			if (ReferenceEquals (array, null))
+				throw new ArgumentNullException ("array");
+
+			return BinarySearch (array, value, 0, array.Length, Comparer<T>.Default.Compare);
+		}
+
+		public static int BinarySearch<T> (T[] array, T value, Comparison<T> compare)
+		{
+			if (ReferenceEquals (array, null))
+				throw new ArgumentNullException ("array");
+			
+			return BinarySearch (array, value, 0, array.Length, compare);
+		}
+			
+		public static int BinarySearch<T> (T[] array, T value, IComparer<T> comparer)
+		{
+			if (ReferenceEquals (array, null))
+				throw new ArgumentNullException ("array");
+			if (ReferenceEquals (comparer, null))
+				throw new ArgumentNullException ("comparer");
+
+			return BinarySearch (array, value, 0, array.Length, comparer.Compare);
+		}
+
+		#endregion
+
+		#region private methods
+
+		private static int BinarySearch<T> (T[] array, T value, int left, int right, Comparison<T> compare)
+		{
+			int mid = left + (right - left) / 2;
+			if (left >= right || array.Length == 0)
+				return -1;
+
+			if (compare (array [mid], value) == 0)
+				return mid;
+
+			if (compare (array [mid], value) > 0)
+				return BinarySearch (array, value, left, mid, compare);
+			return BinarySearch (array, value, mid + 1, right, compare);
+		}
+
+		#endregion
+	}
+}
+
---
+++ Task4.Tests/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using Task4.Lib;
+
+namespace Task4.Tests
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			Assert.AreEqual (0, ArrayExtension.BinarySearch (new int[] { 2, 3, 4 }, 2));
+		}
+
+		[Test ()]
+		public void TestCase1 ()
+		{
+			Assert.AreEqual (2, ArrayExtension.BinarySearch (new int[] { 4, 3, 2 }, 2, 
+				compare: (a, b) => a == b ? 0 : (a > b ? -1 : 1)));
+		}
+	}
+}
+
